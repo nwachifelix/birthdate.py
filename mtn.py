e86ca7d6dd7d525bb675a222cc07863b2a47b48a
nwachifelix <ogbonna54615@gmail.com>
2021-11-14 08:58:51 +0100

    familiarizing with git

---
+++ mtn.py
@@ -0,0 +1,13 @@
+
+# ...List of MTN monthly data plans...
+
+print('(1) N1000 for 1.5GB\n(2) N1200 for 2GB\n(3) N1500 for 3GB\n(4) N2000 for 4.5GB\n(5) N2500 for 6GB\n(6) N3500 for 12GB\n(7) N1000 for 1.5GB+1000\n(8) Next\n(9) Back')
+
+
+userfirstnumber = 2
+usersecondnumber = 5
+total_sum = int(userfirstnumber) + int(usersecondnumber)
+
+userfirstnumber = input("what is your first number? ")
+usersecondnumber = input("what is your second number? ")
+print(f"The total of {userfirstnumber} and {usersecondnumber} is {total_sum}")
