2d273a68deda4d385319624818bc99c5fe106816
nwachifelix <ogbonna54615@gmail.com>
2021-11-14 09:28:41 +0100

    This is my story

---
+++ briefstory.py
@@ -0,0 +1,11 @@
+# A normal conversation I usually have with my drivers.
+
+prompt = "Tell me when you done reach the warehouse, and when dey start offloading."
+print(".............................................................................")
+prompt += "\nYou done reach? "
+receipt = input(prompt)
+
+print(f"Oya sign the receipt, snap am send for WhatsApp.")
+print("..............................................................................")
+
+# Shout to Arinze for those beautiful lines.
